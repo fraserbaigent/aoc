6679b8e1c285947fd9f20073d6b24509a3cab83e
Fraser Baigent <fraserbaigent@gmail.com>
2023-12-10 21:42:51 +0000

    2023 Day 10

--- 2023/tasks/10/solution.py
+++ 2023/tasks/10/solution.py
@@ -3,25 +3,81 @@ def get_data(data_file) -> list:
         return [l.strip() for l in infile.readlines()]
 
 
-data = get_data("data.dat")
-
 ##########################################################################
+
+elements = {
+    "|": [(0, 1), (0, -1)],
+    "L": [(-1, 0), (0, 1)],
+    "-": [(1, 0), (-1, 0)],
+    "J": [(1, 0), (0, 1)],
+    "7": [(1, 0), (0, -1)],
+    "F": [(-1, 0), (0, -1)],
+    ".": [],
+    "S": [],
+}
+
+
+def find_start(data):
+    for i, d in enumerate(data):
+        for j, dd in enumerate(d):
+            if dd == "S":
+                return (j, i)
+
+
+def get_surrounding_spots(coordinate, to_direction, data):
+    x, y = coordinate
+    if x < 0 or y < 0 or x >= len(data[0]) or y >= len(data):
+        return list()
+    symbol = data[y][x]
+
+    if symbol == "." or (symbol != "S" and to_direction not in elements[symbol]):
+        return list()
+
+    points = list()
+    for direction in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+        m = (-direction[0], -direction[1])
+        if m == to_direction:
+            continue
+        if symbol == "S" or m in elements[symbol]:
+            points.append(((x, y), direction))
+    return points
+
+
+def solve_with(start_coordinate, data):
+    next_spots = get_surrounding_spots(start_coordinate, None, data)
+    distances = {start_coordinate: 0}
+    while len(next_spots) > 0:
+        coordinate, direction = next_spots.pop(0)
+        next_coordinate = (coordinate[0] + direction[0], coordinate[1] + direction[1])
+        if next_coordinate in distances:
+            break
+
+        spots_to_add = get_surrounding_spots(next_coordinate, direction, data)
+        if len(spots_to_add) > 0:
+            distances[next_coordinate] = distances[coordinate] + 1
+            next_spots += spots_to_add
+    return distances
 
 
 def part_1(data):
-    total = 0
-
+    start = find_start(data)
+    distances = solve_with(start, data)
+    total = max(distances.values())
     print(f"Part 1: {total}")
     return total
 
 
 def part_2(data):
-    total = 0
-    
+    start = find_start(data)
+    distances = solve_with(start, data)
+    total = area_from(distances)
+
     print(f"Part 2: {total}")
     return total
 
 
 if __name__ == "__main__":
+    data = get_data("data.dat")
+
     part_1(data)
     part_2(data)
--- 2023/tasks/10/test_solution.py
+++ 2023/tasks/10/test_solution.py
@@ -1,11 +1,35 @@
 import solution
+
 test_data = [l.strip() for l in '''
+..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...
+                                
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 8
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
     expected_part_2 = 0
     assert solution.part_2(test_data) == expected_part_2
+
+def test_spots():
+    start_spots = [
+        ((0,2),(1,0)),
+        ((0,2),(0,1)),
+        ((0,2),(-1,0)),
+        ((0,2),(0,-1)),
+        ]
+    assert sorted(solution.get_surrounding_spots((0,2), None, test_data)) == sorted(start_spots)
+
+    assert solution.get_surrounding_spots((0,4),(0,1),test_data) == [((0,4),(1,0))]
+
+    assert solution.get_surrounding_spots((0,4),(-1,0),test_data) == [((0,4),(0,-1))]
+
+    assert solution.get_surrounding_spots((1,4),(1,0),test_data) == [((1,4),(0,-1))]
+
+    assert solution.get_surrounding_spots((1,4),(0,1),test_data) == [((1,4),(-1,0))]
