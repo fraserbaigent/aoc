27ca3452b334258a7271b97e410695196aca87a5
Fraser Baigent <fraserbaigent@gmail.com>
2023-12-10 21:42:25 +0000

    2023 Day 07

--- 2023/tasks/07/test_solution.py
+++ 2023/tasks/07/test_solution.py
@@ -1,11 +1,16 @@
 import solution
 test_data = [l.strip() for l in '''
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 6440
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 5905
     assert solution.part_2(test_data) == expected_part_2
