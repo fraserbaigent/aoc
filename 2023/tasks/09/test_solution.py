2af9e787f91395dbb2c097ceeee0a3d8aadd59c1
Fraser Baigent <fraserbaigent@gmail.com>
2023-12-10 21:42:42 +0000

    2023 Day 09

--- 2023/tasks/09/test_solution.py
+++ 2023/tasks/09/test_solution.py
@@ -1,11 +1,14 @@
 import solution
 test_data = [l.strip() for l in '''
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 114
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 2
     assert solution.part_2(test_data) == expected_part_2
