436f8219f0e3fc4be10d573c5190c55793bea38b
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-01 10:41:33 +0000

    2024 Day 01

--- 2024/01/solution.py
+++ 2024/01/solution.py
@@ -7,11 +7,37 @@ data = get_data("data.dat")
 
 ##########################################################################
 
+
+def parse_data(data):
+    left = list()
+    right = list()
+    for d in data:
+        a = d.split()
+        left.append(int(a[0]))
+        right.append(int(a[1]))
+    return zip(sorted(left), sorted(right))
+
+
 def solve_part_1(data):
-    pass
+    parsed = parse_data(data)
+    return sum([abs(a[0] - a[1]) for a in parsed])
+
 
 def solve_part_2(data):
-    pass
+    parsed = parse_data(data)
+    commonalities = dict()
+    left = list()
+    for p in parsed:
+        left.append(p[0])
+        v = p[1]
+        if v not in commonalities:
+            commonalities[v] = 0
+        commonalities[v] += 1
+    total = 0
+    for p in left:
+        total += p * commonalities.get(p, 0)
+    return total
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +47,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
