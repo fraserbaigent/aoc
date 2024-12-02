8d50d73e78da0f7df7cdb60f9d96bdef01aac4bd
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-02 07:04:37 +0000

    2024 Day 02

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -7,11 +7,54 @@ data = get_data("data.dat")
 
 ##########################################################################
 
+
 def solve_part_1(data):
-    pass
+
+    total = 0
+    for row, d in enumerate(data):
+        v = [int(i) for i in d.strip().split()]
+        good = True
+        decreasing = (v[1] - v[0]) < 0
+        if v[1] - v[0] == 0:
+            continue
+        for i in range(0, len(v) - 1):
+            this_d = v[i + 1] - v[i]
+            if (this_d < 0) != decreasing or this_d == 0 or abs(this_d) > 3:
+                good = False
+                break
+
+        if good:
+            total += 1
+    return total
+
+
+def row_is_good(v):
+    good = True
+    decreasing = (v[1] - v[0]) < 0
+    if v[1] - v[0] == 0:
+        return False
+    for i in range(0, len(v) - 1):
+        this_d = v[i + 1] - v[i]
+        if (this_d < 0) != decreasing or this_d == 0 or abs(this_d) > 3:
+            good = False
+            break
+
+    return good
+
 
 def solve_part_2(data):
-    pass
+    total = 0
+    for row, d in enumerate(data):
+        dat = [int(i) for i in d.strip().split()]
+        good = row_is_good(dat)
+        for i in range(len(dat)):
+            good = good or row_is_good([d_i for i_, d_i in enumerate(dat) if i_ != i])
+            if good:
+                break
+
+        total += 1 if good else 0
+    return total
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +64,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/02/test_solution.py
+++ 2024/02/test_solution.py
@@ -1,11 +1,17 @@
 import solution
 test_data = [l.strip() for l in '''
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9                                
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 2
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 4
     assert solution.part_2(test_data) == expected_part_2
