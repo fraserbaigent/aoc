e7825e8d85afe48d7a9337d12d324a2321babe14
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-04 09:12:42 +0000

    2024 Day 04

--- 2024/04/solution.py
+++ 2024/04/solution.py
@@ -1,17 +1,77 @@
-def get_data(data_file) -> list:
-    with open(data_file, "r") as infile:
-        return [l.strip() for l in infile.readlines()]
+from AocCommon import data_to_list_grid, get_data
 
-
-data = get_data("data.dat")
+data = get_data()
+data = data_to_list_grid(data)
 
 ##########################################################################
 
+
 def solve_part_1(data):
-    pass
+    word = "XMAS"
+    mapping = {i: set() for i in word}
+    for y in range(len(data)):
+        for x in range(len(data[0])):
+            if data[y][x] in mapping:
+                mapping[data[y][x]].add((x, y))
+
+    good = 0
+    dirs = [(x, y) for x in [-1, 0, 1] for y in [-1, 0, 1]]
+    for x0, y0 in mapping[word[0]]:
+        for dx, dy in dirs:
+            this_good = True
+            for i in range(1, len(word)):
+                x = x0 + dx * i
+                y = y0 + dy * i
+                if (
+                    x < 0
+                    or x >= len(data[0])
+                    or y < 0
+                    or y >= len(data)
+                    or data[y][x] != word[i]
+                ):
+                    this_good = False
+                    break
+            if this_good == True:
+                good += 1
+    return good
+
 
 def solve_part_2(data):
-    pass
+    total = 0
+    for y in range(len(data)):
+        for x in range(len(data[0])):
+            if data[y][x] == "A":
+                good = True
+                for i in [-1, 1]:
+                    x1 = x - 1
+                    y1 = y + i
+                    x2 = x + 1
+                    y2 = y - i
+                    if (
+                        x1 < 0
+                        or x2 >= len(data[0])
+                        or y1 < 0
+                        or y2 < 0
+                        or y1 >= len(data)
+                        or y2 >= len(data)
+                    ):
+                        good = False
+                        break
+
+                    c1 = data[y1][x1]
+                    c2 = data[y2][x2]
+
+                    if c1 == "M" and c2 == "S":
+                        continue
+                    elif c1 == "S" and c2 == "M":
+                        continue
+                    else:
+                        good = False
+                        break
+                if good:
+                    total += 1
+    return total
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +81,6 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
     print(f"Part 2: {total}")
     return total
 
--- 2024/04/test_solution.py
+++ 2024/04/test_solution.py
@@ -1,11 +1,21 @@
 import solution
-test_data = [l.strip() for l in '''
-'''.split('\n') if len(l.strip())]
+from AocCommon import data_to_list_grid, get_data
+
+test_data = data_to_list_grid([l.strip() for l in '''MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX'''.split('\n') if len(l.strip())])
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 18
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 9
     assert solution.part_2(test_data) == expected_part_2
