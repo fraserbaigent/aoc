11694b17258ff4c264deb98b0d1f7b733490b8b1
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-06 09:03:05 +0000

    2024 Day 05

--- 2024/05/solution.py
+++ 2024/05/solution.py
@@ -7,11 +7,83 @@ data = get_data("data.dat")
 
 ##########################################################################
 
+
+def parse_inputs(data):
+    mappings = dict()
+    commands = list()
+    for d in data:
+        if len(d) == 0:
+            continue
+        if "|" in d:
+            v = [int(a) for a in d.split("|")]
+            if v[0] not in mappings:
+                mappings[v[0]] = set()
+            mappings[v[0]].add(v[1])
+        else:
+            commands.append([int(a) for a in d.split(",")])
+    return mappings, commands
+
+
+def get_good_lines(mappings, commands):
+    good_lines = list()
+    for c in commands:
+        good = True
+        for i in range(0, len(c) - 1):
+            for j in range(i + 1, len(c)):
+                if not (
+                    c[j] in mappings.get(c[i], []) or c[i] not in mappings.get(c[j], [])
+                ):
+                    good = False
+
+        if good == True:
+            good_lines.append(c)
+    return good_lines
+
+
 def solve_part_1(data):
-    pass
+    mappings, commands = parse_inputs(data)
+    good_lines = get_good_lines(mappings, commands)
+
+    total = 0
+    for g in good_lines:
+        total += g[len(g) // 2]
+    return total
+
+
+def correct(mappings, bad_lines):
+    for b in bad_lines:
+        i = 0
+        while i < len(b) - 1:
+            j = i + 1
+            swapped = False
+            while j < len(b):
+                left = b[i]
+                right = b[j]
+                left_mapping = mappings.get(left, [])
+                right_mapping = mappings.get(right, [])
+                if right in left_mapping or left not in right_mapping:
+                    j += 1
+                    continue
+                b[i] = right
+                b[j] = left
+                swapped = True
+                break
+            if swapped == False:
+                i += 1
+    return bad_lines
+
 
 def solve_part_2(data):
-    pass
+    mappings, commands = parse_inputs(data)
+    good_lines = get_good_lines(mappings, commands)
+    bad_lines = [c for c in commands if c not in good_lines]
+
+    corrected_bad_lines = correct(mappings, bad_lines)
+    total = 0
+    for g in bad_lines:
+        total += g[len(g) // 2]
+    return total
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +93,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/05/test_solution.py
+++ 2024/05/test_solution.py
@@ -1,11 +1,39 @@
 import solution
 test_data = [l.strip() for l in '''
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47                                
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 143
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 123
     assert solution.part_2(test_data) == expected_part_2
