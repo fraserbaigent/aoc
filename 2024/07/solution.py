1f669e5b83d2f8c66cdcb3cb2c125d9768406ab5
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-07 10:19:26 +0000

    2024 Day 07

--- 2024/07/solution.py
+++ 2024/07/solution.py
@@ -1,17 +1,66 @@
-def get_data(data_file) -> list:
-    with open(data_file, "r") as infile:
-        return [l.strip() for l in infile.readlines()]
+import re
 
+from AocCommon import get_data
 
-data = get_data("data.dat")
+data = get_data()
 
 ##########################################################################
 
+
+def parse_data(data):
+    parsed = []
+    regex = re.compile("(\\d+): (.*)")
+    for d in data:
+        res = regex.match(d)
+        parsed.append((int(res.groups()[0]), [int(i) for i in res.groups()[1].split()]))
+
+    return parsed
+
+
+def recurse(total, values, operator, operators):
+    if operator == operators[0]:
+        val = sum(values[:2])
+    elif operator == operators[1]:
+        val = values[0] * values[1]
+    elif len(operators) > 2 and operator == operators[2]:
+        val = int(f"{values[0]}{values[1]}")
+
+    if len(values) == 2:
+        return val == total
+    else:
+        new_values = [val]
+        for v in values[2:]:
+            new_values.append(v)
+        for o in operators:
+            if recurse(total, new_values, o, operators):
+                return True
+        return False
+
+
+def is_true(total, values, operators):
+    for o in operators:
+        if recurse(total, values, o, operators):
+            return True
+    return False
+
+
 def solve_part_1(data):
-    pass
+    parsed_data = parse_data(data)
+    total = 0
+    for t, v in parsed_data:
+        if is_true(t, v, ["+", "*"]):
+            total += t
+    return total
+
 
 def solve_part_2(data):
-    pass
+    parsed_data = parse_data(data)
+    total = 0
+    for t, v in parsed_data:
+        if is_true(t, v, ["+", "*", "||"]):
+            total += t
+    return total
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +70,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/07/test_solution.py
+++ 2024/07/test_solution.py
@@ -1,11 +1,18 @@
 import solution
-test_data = [l.strip() for l in '''
-'''.split('\n') if len(l.strip())]
+test_data = [l.strip() for l in '''190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20'''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 3749
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 11387
     assert solution.part_2(test_data) == expected_part_2
