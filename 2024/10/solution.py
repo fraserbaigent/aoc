1a544c5da6d979a8857dced79ef5f206e012e85c
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-10 21:19:18 +0000

    2024 Day 10

--- 2024/10/solution.py
+++ 2024/10/solution.py
@@ -1,17 +1,59 @@
-def get_data(data_file) -> list:
-    with open(data_file, "r") as infile:
-        return [l.strip() for l in infile.readlines()]
+from AocCommon import get_data, data_to_list_grid
 
-
-data = get_data("data.dat")
+data = get_data()
+data = data_to_list_grid(data, ty = int)
 
 ##########################################################################
 
+DIRS = [(-1,0),(1,0),(0,1),(0,-1)]
+
 def solve_part_1(data):
-    pass
+    memo = [[None for i in d] for d in data]
+    total = 0
+    for y, d in enumerate(data):
+        for x, d_i in enumerate(d):
+            if d_i == 0:
+                v = recurse((x,y), None, memo, data)
+                total += len(v[0])
+    return total
+
+def recurse(coord, direction, memo, grid):
+    x0,y0 = coord
+    total = 0
+    if grid[y0][x0] == 9:
+        memo[y0][x0] = (set([(x0,y0)]), 1)
+    
+    if memo[y0][x0] is not None:
+        return memo[y0][x0]
+
+    ends = set()
+    total = 0
+    for dx, dy in DIRS:
+        if direction is not None and (-dx,-dy) == direction:
+            continue
+        x = x0 + dx
+        y = y0 + dy
+        if x < 0 or y < 0 or x >= len(grid[0]) or y >= len(grid):
+            continue
+
+        if grid[y][x] == grid[y0][x0] + 1:
+            v, t = recurse((x,y), (dx,dy), memo, grid)
+            for v_i in v:
+                ends.add(v_i)
+            total += t
+    memo[y0][x0] = (ends, total)
+    return ends, total
+        
 
 def solve_part_2(data):
-    pass
+    memo = [[None for i in d] for d in data]
+    total = 0
+    for y, d in enumerate(data):
+        for x, d_i in enumerate(d):
+            if d_i == 0:
+                v = recurse((x,y), None, memo, data)
+                total += v[1]
+    return total
 
 def part_1(data):
     total = solve_part_1(data)
--- 2024/10/test_solution.py
+++ 2024/10/test_solution.py
@@ -1,11 +1,63 @@
+def get_test_data(test_str):
+    return [
+        [int(i) if i != '.' else None for i in d] for d in 
+        test_str.split("\n")]    
 import solution
-test_data = [l.strip() for l in '''
-'''.split('\n') if len(l.strip())]
+
+test_data = get_test_data(
+'''...0...
+...1...
+...2...
+6543456
+7.....7
+8.....8
+9.....9''')
+
+test_data_2 = get_test_data(
+'''..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....''')
+
+test_data_3 = get_test_data(
+'''10..9..
+2...8..
+3...7..
+4567654
+...8..3
+...9..2
+.....01''')
+
+test_data_4 = get_test_data(
+    '''89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732''')
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 2
     assert solution.part_1(test_data) == expected_part_1
 
+def test_part_12():
+    expected_part_1 = 4
+    assert solution.part_1(test_data_2) == expected_part_1
+
+def test_part_13():
+    expected_part_1 = 3
+    assert solution.part_1(test_data_3) == expected_part_1
+
+def test_part_14():
+    expected_part_1 = 36
+    assert solution.part_1(test_data_4) == expected_part_1
+
+
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = None
     assert solution.part_2(test_data) == expected_part_2
