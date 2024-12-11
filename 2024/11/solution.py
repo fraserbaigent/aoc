78ce23a4e7ef5ddbbe1ea484641c228235a9405f
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-11 08:27:38 +0000

    2024 Day 11

--- 2024/11/solution.py
+++ 2024/11/solution.py
@@ -1,27 +1,66 @@
-def get_data(data_file) -> list:
-    with open(data_file, "r") as infile:
-        return [l.strip() for l in infile.readlines()]
+from AocCommon import get_data_blob
 
-
-data = get_data("data.dat")
+data = get_data_blob()
+data = data.split()
 
 ##########################################################################
 
-def solve_part_1(data):
-    pass
 
-def solve_part_2(data):
-    pass
+def split_stone(stone, times):
+    to_add = [stone]
+    next_to_add = []
+    for i in range(0, times):
+        while len(to_add) > 0:
+            s = to_add.pop(0)
+            if int(s) == 0:
+                next_to_add.append("1")
+            elif len(s) % 2 == 0:
+                next_to_add.append(str(int(s[: len(s) // 2])))
+                next_to_add.append(str(int(s[len(s) // 2 :])))
+            else:
+                next_to_add.append(str(int(s) * 2024))
+
+        to_add = next_to_add
+        next_to_add = []
+    return to_add
+
+
+def solve_with_memo(stone, index, memo):
+    if (stone, index) in memo:
+        return memo[(stone, index)]
+
+    total = 0
+    if index == 75:
+        total = 1
+    elif int(stone) == 0:
+        total += solve_with_memo("1", index + 1, memo)
+    elif len(stone) % 2 == 0:
+        for s in [
+            str(int(stone[: len(stone) // 2])),
+            str(int(stone[len(stone) // 2 :])),
+        ]:
+            total += solve_with_memo(s, index + 1, memo)
+    else:
+        total += solve_with_memo(str(int(stone) * 2024), index + 1, memo)
+
+    memo[(stone, index)] = total
+    return total
+
 
 def part_1(data):
-    total = solve_part_1(data)
+    total = 0
+    for d in data:
+        total += len(split_stone(d, 25))
     print(f"Part 1: {total}")
     return total
 
 
 def part_2(data):
-    total = solve_part_2(data)
-    
+    total = 0
+    memo = {}
+    for d in data:
+        total += solve_with_memo(d, 0, memo)
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/11/test_solution.py
+++ 2024/11/test_solution.py
@@ -3,8 +3,9 @@ test_data = [l.strip() for l in '''
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
-    assert solution.part_1(test_data) == expected_part_1
+    test_data = "125 17".split()
+    assert solution.solve_part_1(test_data, 6) == 22
+    assert solution.solve_part_1(test_data, 25) == 55312
 
 def test_part_2():
     expected_part_2 = 0
