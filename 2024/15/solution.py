d1d1578e96ef466c4e86aa30e7ee0210441ecee4
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-15 09:53:50 +0000

    2024 Day 15

--- 2024/15/solution.py
+++ 2024/15/solution.py
@@ -7,11 +7,139 @@ data = get_data("data.dat")
 
 ##########################################################################
 
+
+def parse_data(data, is_part_2=False):
+    grid = []
+    instructions = None
+
+    for i, d in enumerate(data):
+        if len(d) == 0:
+            instructions = "".join(data[i:]).replace("\n", "")
+            break
+        else:
+            if is_part_2 == True:
+                d = (
+                    d.replace("#", "##")
+                    .replace("O", "[]")
+                    .replace(".", "..")
+                    .replace("@", "@.")
+                )
+
+            grid.append([c for c in d])
+
+    return grid, instructions
+
+
+def find_start(grid):
+    for y, g in enumerate(grid):
+        for x, c in enumerate(g):
+            if grid[y][x] == "@":
+                return x, y
+    return None, None
+
+
+DIRS = {
+    "^": (0, -1),
+    "v": (0, 1),
+    ">": (1, 0),
+    "<": (-1, 0),
+}
+
+
+def move(x, y, direction, grid):
+    dx, dy = DIRS[direction]
+    nx = x + dx
+    ny = y + dy
+    if grid[ny][nx] == ".":
+        grid[ny][nx] = grid[y][x]
+        if grid[y][x] == "@":
+            grid[y][x] = "."
+        return nx, ny
+    elif grid[ny][nx] == "#":
+        return x, y
+    elif grid[ny][nx] == "O" or grid[ny][nx] in "[]":
+        nnx, nny = move(nx, ny, direction, grid)
+        if nnx == nx and nny == ny:
+            return x, y
+        else:
+            grid[ny][nx] = grid[y][x]
+            if grid[y][x] == "@":
+                grid[y][x] = "."
+            return nx, ny
+
+
+def get_score(grid, is_part_2=False):
+    total = 0
+    for y, row in enumerate(grid):
+        for x, c in enumerate(row):
+            if c == "O" or (is_part_2 and c == "["):
+                v = (y * 100) + x
+                total += v
+    return total
+
+
 def solve_part_1(data):
-    pass
+    grid, instructions = parse_data(data)
+    x, y = find_start(grid)
+    for c in instructions:
+        x, y = move(x, y, c, grid)
+    return get_score(grid)
+
+
+def move_2(x, y, direction, grid):
+    dx, dy = DIRS[direction]
+    if dy == 0:
+        x, y = move(x, y, direction, grid)
+        return x, y
+    else:
+        valid_moves = move_2_vertical(x, y, direction, grid)
+        if len(valid_moves) == 0:
+            return x, y
+        visited = []
+        for (x_f, y_f), (x_t, y_t), c in valid_moves:
+            grid[y_t][x_t] = c
+            visited.append((x_t, y_t))
+        for c_from, _, _ in valid_moves:
+            if c_from not in visited:
+                x_f, y_f = c_from
+                grid[y_f][x_f] = "."
+
+        return x, y + dy
+
+
+def move_2_vertical(x, y, direction, grid):
+    dx, dy = DIRS[direction]
+    ny = y + dy
+    if grid[ny][x] == "#":
+        return []
+    elif grid[ny][x] == ".":
+        return [((x, y), (x, ny), grid[y][x])]
+
+    if grid[ny][x] == "[":
+        nx = x + 1
+    elif grid[ny][x] == "]":
+        nx = x - 1
+    else:
+        raise Exception(f"WTF - {x}, {ny} is {grid[ny][x]}")
+    valid = [((x, y), (x, ny), grid[y][x])]
+    for x_i in [x, nx]:
+        next_row = move_2_vertical(x_i, ny, direction, grid)
+        if len(next_row) == 0:
+            return []
+        else:
+            for n in next_row:
+                valid.append(n)
+
+    return valid
+
 
 def solve_part_2(data):
-    pass
+    grid, instructions = parse_data(data, True)
+    x, y = find_start(grid)
+    for i, c in enumerate(instructions):
+        x, y = move_2(x, y, c, grid)
+    return get_score(grid, True)
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +149,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/15/test_solution.py
+++ 2024/15/test_solution.py
@@ -1,11 +1,30 @@
 import solution
-test_data = [l.strip() for l in '''
-'''.split('\n') if len(l.strip())]
+test_data = [l.strip() for l in '''##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^'''.split('\n')]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 10092
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 9021
     assert solution.part_2(test_data) == expected_part_2
