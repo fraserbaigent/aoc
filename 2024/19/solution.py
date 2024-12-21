60b2130b7115405e32590dc37150a4373a5a39e5
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-21 19:07:43 +0000

    2024 Day 19

--- 2024/19/solution.py
+++ 2024/19/solution.py
@@ -7,21 +7,46 @@ data = get_data("data.dat")
 
 ##########################################################################
 
-def solve_part_1(data):
-    pass
+
+def solve_part_1(instructions, string, index, cache):
+    total = 0
+    if string[index:] in cache:
+        return cache[string[index:]]
+
+    for i in instructions:
+        if index + len(i) - 1 >= len(string):
+            continue
+        substr = string[index : index + len(i)]
+        if substr == i:
+            if index + len(i) == len(string):
+                total += 1
+            else:
+                total += solve_part_1(instructions, string, index + len(i), cache)
+    cache[string[index:]] = total
+    return total
+
 
 def solve_part_2(data):
     pass
 
+
 def part_1(data):
-    total = solve_part_1(data)
+    instructions = data[0].split(", ")
+    total = 0
+    for d in data[2:]:
+        t = solve_part_1(instructions, d, 0, {})
+        if t > 0:
+            total += 1
     print(f"Part 1: {total}")
     return total
 
 
 def part_2(data):
-    total = solve_part_2(data)
-    
+    instructions = data[0].split(", ")
+    total = 0
+    for d in data[2:]:
+        total += solve_part_1(instructions, d, 0, {})
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/19/test_solution.py
+++ 2024/19/test_solution.py
@@ -1,11 +1,19 @@
 import solution
-test_data = [l.strip() for l in '''
-'''.split('\n') if len(l.strip())]
+test_data = [l.strip() for l in '''r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb'''.split('\n')]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 6
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
+    expected_part_2 = 16
     assert solution.part_2(test_data) == expected_part_2
