f25b5c633a35baaf1e6b66a6ca5e4863e2cfe523
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-21 19:07:36 +0000

    2024 Day 17

--- 2024/17/solution.py
+++ 2024/17/solution.py
@@ -1,3 +1,4 @@
+import math
 def get_data(data_file) -> list:
     with open(data_file, "r") as infile:
         return [l.strip() for l in infile.readlines()]
@@ -7,20 +8,86 @@ data = get_data("data.dat")
 
 ##########################################################################
 
-def solve_part_1(data):
-    pass
+def solve_part_1(a,b,c,commands):
+    index = 0
+    outputs = list()
+    iterations = 0
+    while index < len(commands):# and iterations < 1e7:
+        increase = True
+        ins = commands[index]
+        op = commands[index+1]
+        if op <= 3:
+            v = op
+        elif op == 4:
+            v =a
+        elif op == 5:
+            v=b
+        elif op == 6:
+            v = c
+        elif op == 7:
+            v=None
 
-def solve_part_2(data):
-    pass
+        if ins == 0:
+            a = int(math.trunc(a / float(2 ** v)))
+        elif ins == 1:
+            b = b ^ op
+        elif ins == 2:
+            b = v % 8
+        elif ins == 3:
+            if a != 0:
+                index = op
+                increase = False
+        elif ins == 4:
+            b = b ^ c
+        elif ins == 5:
+            outputs.append(v % 8)
+        elif ins == 6:
+            b = int(math.trunc(a / (2. ** v)))
+        elif ins == 7:
+            c = int(math.trunc(a / (2. ** v)))
+        
+        if increase:
+            index += 2
+        iterations+=1
+            
+    return outputs
 
+def solve_part_2(input_commands):
+    a = 50230824
+    i = 0
+    longest = 0
+    while True:
+        i+=1
+        outputs = solve_part_1(a,0,0,input_commands)
+        if len(outputs) < len(input_commands):
+            print(len(outputs), i)
+            a*=8
+        elif len(outputs) < len(input_commands):
+            a/= 8
+        else:
+            a+=1
+        longest = max(longest, len(outputs))
+        print(",".join([str(o) for o in input_commands]))
+        print(",".join([str(o) for o in outputs]))
+        if outputs == input_commands:
+            break
+    
+    return a
+    
 def part_1(data):
-    total = solve_part_1(data)
-    print(f"Part 1: {total}")
-    return total
+    a = int(data[0].split(':')[-1])
+    b = int(data[1].split(':')[-1])
+    c = int(data[2].split(':')[-1])
+    commands = [int(i) for i in data[-1].split(':')[1].split(',')]
+    
+    outputs = solve_part_1(a,b,c,commands)
+    answer = ",".join([str(i) for i in outputs])    
+    print(f"Part 1: {answer}")
 
 
 def part_2(data):
-    total = solve_part_2(data)
+    commands = [int(i) for i in data[-1].split(':')[1].split(',')]    
+    total = solve_part_2(commands)
     
     print(f"Part 2: {total}")
     return total
--- 2024/17/test_solution.py
+++ 2024/17/test_solution.py
@@ -3,9 +3,9 @@ test_data = [l.strip() for l in '''
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
-    assert solution.part_1(test_data) == expected_part_1
+    expected_part_1 = "4,6,3,5,6,3,5,2,1,0"
+    assert solution.solve_part_1(729,0,0,[0,1,5,4,3,0]) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
-    assert solution.part_2(test_data) == expected_part_2
+    expected_part_2 = "0,3,5,4,3,0"
+    assert solution.solve_part_2([0,3,5,4,3,0]) == expected_part_2
