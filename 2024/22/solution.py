8d81d5bf0ccc53436d8db581423b555cde886e75
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-22 12:20:17 +0000

    2024 Day 22

--- 2024/22/solution.py
+++ 2024/22/solution.py
@@ -1,3 +1,8 @@
+import math
+
+from AocCommon import submit
+
+
 def get_data(data_file) -> list:
     with open(data_file, "r") as infile:
         return [l.strip() for l in infile.readlines()]
@@ -7,11 +12,74 @@ data = get_data("data.dat")
 
 ##########################################################################
 
+
+def mix(one, two):
+    return one ^ two
+
+
+def prune(number):
+    return number % 16777216
+
+
+def generate_secret(original):
+    updated = mix(original, original * 64)
+    updated = prune(updated)
+
+    updated = mix(updated, int(math.floor(updated / 32.0)))
+    updated = prune(updated)
+
+    updated = mix(updated, updated * 2048)
+    return prune(updated)
+
+
 def solve_part_1(data):
-    pass
+    data = [int(d) for d in data]
+    total = 0
+    for d in data:
+        for i in range(0, 2000):
+            d = generate_secret(d)
+        total += d
+    return total
+
 
 def solve_part_2(data):
-    pass
+    data = [int(d) for d in data]
+    sells = []
+    print(data)
+    for d in data:
+        sequence = []
+        sell_data = {}
+        previous = int(str(d)[-1])
+        for i in range(0, 2000):
+            d = generate_secret(d)
+            price = int(str(d)[-1])
+
+            delta = price - previous
+            previous = price
+            sequence.append(delta)
+            if len(sequence) > 4:
+                sequence.pop(0)
+            if len(sequence) == 4:
+                key = tuple(sequence)
+                if key not in sell_data:
+                    sell_data[key] = price
+        sells.append(sell_data)
+
+    options = set()
+    for s in sells:
+        options |= set(s.keys())
+
+    total = 0
+    max_path = None
+    for o in options:
+        tot = 0
+        for s in sells:
+            tot += s.get(o, 0)
+        if tot > total:
+            max_path = o
+        total = max(tot, total)
+    return total
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +89,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/22/test_solution.py
+++ 2024/22/test_solution.py
@@ -1,11 +1,21 @@
 import solution
-test_data = [l.strip() for l in '''
+test_data = [int(l.strip()) for l in '''1
+10
+100
+2024
+
 '''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 37327623
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
-    expected_part_2 = 0
-    assert solution.part_2(test_data) == expected_part_2
+    expected_part_2 = 23
+    assert solution.part_2([1,2,3,2024]) == expected_part_2
+
+def test_prune():
+    assert 16113920 == solution.prune(100000000)
+
+def test_mix():
+    assert 37 == solution.mix(42, 15)
