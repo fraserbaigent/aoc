1ca2e027c71f36b8a013ad58428f17f32d5eb14f
Fraser Baigent <fraserbaigent@gmail.com>
2024-12-22 12:20:25 +0000

    2024 Day 20

--- 2024/20/solution.py
+++ 2024/20/solution.py
@@ -1,3 +1,6 @@
+import json
+
+
 def get_data(data_file) -> list:
     with open(data_file, "r") as infile:
         return [l.strip() for l in infile.readlines()]
@@ -7,11 +10,75 @@ data = get_data("data.dat")
 
 ##########################################################################
 
+
+def get_start_end(data):
+    for y, row in enumerate(data):
+        for x, c in enumerate(row):
+            if c == "E":
+                end = (x, y)
+            elif c == "S":
+                start = (x, y)
+    return start, end
+
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
 def solve_part_1(data):
-    pass
+    start, end = get_start_end(data)
+    to_end = dict()
+
+    checks = [(end, 0)]
+
+    def in_bounds(x_, y_):
+        return x_ >= 0 and y_ >= 0 and y_ < len(data) and x_ < len(data[0])
+
+    while len(checks):
+        (x, y), s = checks.pop(0)
+        to_end[(x, y)] = s
+        for dx, dy in DIRS:
+            xx = x + dx
+            yy = y + dy
+            if not in_bounds(xx, yy) or data[yy][xx] == "#" or (xx, yy) in to_end:
+                continue
+
+            checks.append(((xx, yy), s + 1))
+
+    scores = {}
+    for x, y in to_end:
+        to_check = list()
+        for dx, dy in DIRS:
+            xx = x + dx
+            yy = y + dx
+            if not in_bounds(xx, yy) or data[yy][xx] != "#":
+                continue
+            for dxx, dyy in DIRS:
+                xxx = xx + dxx
+                yyy = yy + dyy
+                if (
+                    not in_bounds(xxx, yyy)
+                    or (xxx, yyy) == (x, y)
+                    or data[yyy][xxx] == "#"
+                ):
+                    continue
+                to_check.append((xxx, yyy))
+        for t in to_check:
+            other = to_end[t]
+            saving = to_end[(x, y)] - (other)
+            if saving > 0:
+                if saving not in scores:
+                    scores[saving] = 0
+                scores[saving] += 1
+    for k in sorted(list(scores.keys())):  # , key=lambda k: scores[k]):
+        print(k, scores[k])
+
+    #    print(json.dumps(scores, indent=4))
+    return sum([1 for s, v in scores.items() if v >= 100])
+
 
 def solve_part_2(data):
     pass
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +88,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/20/test_solution.py
+++ 2024/20/test_solution.py
@@ -1,9 +1,22 @@
 import solution
-test_data = [l.strip() for l in '''
-'''.split('\n') if len(l.strip())]
+test_data = [l.strip() for l in '''###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############'''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 123123
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
