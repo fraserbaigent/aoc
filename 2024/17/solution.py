51c30cc20ec446a59565d238f2f61d6c027edf3a
Fraser Baigent <fraserbaigent@gmail.com>
2025-01-01 19:36:34 +0000

    2024 Day 17

--- 2024/17/solution.py
+++ 2024/17/solution.py
@@ -1,4 +1,6 @@
 import math
+
+
 def get_data(data_file) -> list:
     with open(data_file, "r") as infile:
         return [l.strip() for l in infile.readlines()]
@@ -8,29 +10,30 @@ data = get_data("data.dat")
 
 ##########################################################################
 
-def solve_part_1(a,b,c,commands):
+
+def solve_part_1(a, b, c, commands):
     index = 0
     outputs = list()
     iterations = 0
-    while index < len(commands):# and iterations < 1e7:
+    while index < len(commands):  # and iterations < 1e7:
         increase = True
         ins = commands[index]
-        op = commands[index+1]
+        op = commands[index + 1]
         if op <= 3:
             v = op
         elif op == 4:
-            v =a
+            v = a
         elif op == 5:
-            v=b
+            v = b
         elif op == 6:
             v = c
         elif op == 7:
-            v=None
+            v = None
 
         if ins == 0:
-            a = int(math.trunc(a / float(2 ** v)))
+            a = int(math.trunc(a / float(2**v)))
         elif ins == 1:
-            b = b ^ op
+            b = int(b) ^ op
         elif ins == 2:
             b = v % 8
         elif ins == 3:
@@ -42,53 +45,78 @@ def solve_part_1(a,b,c,commands):
         elif ins == 5:
             outputs.append(v % 8)
         elif ins == 6:
-            b = int(math.trunc(a / (2. ** v)))
+            b = int(math.trunc(a / (2.0**v)))
         elif ins == 7:
-            c = int(math.trunc(a / (2. ** v)))
-        
+            c = int(math.trunc(a / (2.0**v)))
+
         if increase:
             index += 2
-        iterations+=1
-            
+        iterations += 1
+
     return outputs
+
 
 def solve_part_2(input_commands):
     a = 50230824
-    i = 0
+    ind = 0
     longest = 0
+    matched = list()
     while True:
-        i+=1
-        outputs = solve_part_1(a,0,0,input_commands)
+        outputs = solve_part_1(a, 0, 0, input_commands)
+        a = int(a)
         if len(outputs) < len(input_commands):
-            print(len(outputs), i)
-            a*=8
-        elif len(outputs) < len(input_commands):
-            a/= 8
+            a *= 8
+        elif len(outputs) > len(input_commands):
+            a /= 8
         else:
-            a+=1
-        longest = max(longest, len(outputs))
-        print(",".join([str(o) for o in input_commands]))
-        print(",".join([str(o) for o in outputs]))
-        if outputs == input_commands:
+            a += int(int(a) // 8)
+
+        if len(outputs) == len(input_commands):
+            p = len(matched)
+            m = 0
+            for i in range(len(input_commands)):
+                index = len(outputs) - 1 - i
+                if outputs[index] == input_commands[index]:
+                    m += 1
+                else:
+                    break
+            if m > 0:
+                matched.append((a, m))
+            if len(matched) > p:
+                print(
+                    f"{a:<15} ({a %
+                                8}): {matched[-1]} matched:\n{outputs}\n{input_commands}"
+                )
+                ind += 1
+
+        #        print(",".join([str(o) for o in input_commands]))
+        #        print(",".join([str(o) for o in outputs]))
+        if outputs == input_commands or ind >= 10:
             break
-    
+    matched = sorted(matched, key=lambda k: k[0])
+    for i in range(1, len(matched)):
+        m, l = matched[i]
+        delta = m - matched[i - 1][0]
+        print(m, l, delta)
+
     return a
-    
+
+
 def part_1(data):
-    a = int(data[0].split(':')[-1])
-    b = int(data[1].split(':')[-1])
-    c = int(data[2].split(':')[-1])
-    commands = [int(i) for i in data[-1].split(':')[1].split(',')]
-    
-    outputs = solve_part_1(a,b,c,commands)
-    answer = ",".join([str(i) for i in outputs])    
+    a = int(data[0].split(":")[-1])
+    b = int(data[1].split(":")[-1])
+    c = int(data[2].split(":")[-1])
+    commands = [int(i) for i in data[-1].split(":")[1].split(",")]
+
+    outputs = solve_part_1(a, b, c, commands)
+    answer = ",".join([str(i) for i in outputs])
     print(f"Part 1: {answer}")
 
 
 def part_2(data):
-    commands = [int(i) for i in data[-1].split(':')[1].split(',')]    
+    commands = [int(i) for i in data[-1].split(":")[1].split(",")]
     total = solve_part_2(commands)
-    
+
     print(f"Part 2: {total}")
     return total
 
