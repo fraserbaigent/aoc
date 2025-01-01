ce8806e2279660b0e30767c7edeb01f61347fbe5
Fraser Baigent <fraserbaigent@gmail.com>
2025-01-01 11:59:02 +0000

    2024 Day 23

--- 2024/23/solution.py
+++ 2024/23/solution.py
@@ -7,11 +7,54 @@ data = get_data("data.dat")
 
 ##########################################################################
 
+def get_mappings(data):
+
+    mappings = {}
+    for d in data:
+        f, t = tuple(d.split("-"))
+        if f not in mappings:
+            mappings[f] = set()
+        if t not in mappings:
+            mappings[t] = set()
+        mappings[f].add(t)
+        mappings[t].add(f)
+    return mappings
+
 def solve_part_1(data):
-    pass
+    mappings = get_mappings(data)
+
+    sets = set()
+    for m, v in mappings.items():
+        v = list(v)
+        for i in range(0, len(v) - 1):
+            for j in range(i + 1, len(v)):
+                if v[i] in mappings[v[j]]:
+                    sets.add(tuple(sorted([m, v[i], v[j]])))
+
+    total = 0
+    for s in sets:
+        if len(s) != 3:
+            continue
+        for s_ in s:
+            if s_[0] == "t":
+                print(s)
+                total += 1
+                break
+    return total
+
 
 def solve_part_2(data):
-    pass
+    sets = set()
+    for m,v in mappings.items():
+        pass
+
+    m = []
+    for s in sets:
+        if len(s) > m:
+            m=s
+    return ','.join(sorted(list(m)))
+    
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +64,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
--- 2024/23/test_solution.py
+++ 2024/23/test_solution.py
@@ -1,9 +1,39 @@
 import solution
-test_data = [l.strip() for l in '''
-'''.split('\n') if len(l.strip())]
+test_data = [l.strip() for l in '''kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn'''.split('\n') if len(l.strip())]
 
 def test_part_1():
-    expected_part_1 = 0
+    expected_part_1 = 7
     assert solution.part_1(test_data) == expected_part_1
 
 def test_part_2():
