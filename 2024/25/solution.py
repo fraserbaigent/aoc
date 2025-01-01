a4150b172dc8234c1615eba5840c55bdaf08d9c7
Fraser Baigent <fraserbaigent@gmail.com>
2025-01-01 11:58:54 +0000

    2024 Day 25

--- 2024/25/solution.py
+++ 2024/25/solution.py
@@ -1,17 +1,59 @@
+from AocCommon import get_data_blob
+
+
 def get_data(data_file) -> list:
     with open(data_file, "r") as infile:
         return [l.strip() for l in infile.readlines()]
 
 
-data = get_data("data.dat")
+data = get_data_blob()
 
 ##########################################################################
 
+
+def parse(data):
+    keys = []
+    locks = []
+    for k in data.split("\n\n"):
+        lines = [k_.strip() for k_ in k.split("\n") if k_.strip() != ""]
+        is_key = "." in lines[0]
+        height = [0 for i in lines[0]]
+        for l in lines[1:-1]:
+            for i, l_ in enumerate(l):
+                if l_ == "#":
+                    height[i] += 1
+        if is_key:
+            keys.append(height)
+        else:
+            locks.append(height)
+    return keys, locks
+
+
+def fits(key, lock):
+    for i, k in enumerate(key):
+        if k + lock[i] > 5:
+            return False
+    return True
+
+
+def get_total(keys, locks):
+    total = 0
+    for l in locks:
+        for k in keys:
+            if fits(k, l):
+                total += 1
+    return total
+
+
 def solve_part_1(data):
-    pass
+    keys, locks = parse(data)
+    total = get_total(keys, locks)
+    return total
+
 
 def solve_part_2(data):
     pass
+
 
 def part_1(data):
     total = solve_part_1(data)
@@ -21,7 +63,7 @@ def part_1(data):
 
 def part_2(data):
     total = solve_part_2(data)
-    
+
     print(f"Part 2: {total}")
     return total
 
